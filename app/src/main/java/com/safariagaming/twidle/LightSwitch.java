b7dfe72ac034e9ecc5258e260c200a57409d89d2
Nikita <nikita@Nikitas-Mac-mini.local>
2017-03-04 16:06:37 +0300

    Fix OFM bug

--- app/src/main/java/com/safariagaming/twidle/LightSwitch.java
+++ app/src/main/java/com/safariagaming/twidle/LightSwitch.java
@@ -1,8 +1,8 @@
 package com.safariagaming.twidle;
 
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -17,18 +17,16 @@ public class LightSwitch extends AppCompatActivity {
         setContentView(R.layout.activity_light_switch);
     }
 
-    public void switchOn(View view)
-    {
-     on = (ImageButton) findViewById(R.id.light_switch_off);
-     on.setVisibility(View.INVISIBLE);
-     onSound = MediaPlayer.create(this, R.raw.open_light_switch_sound);
-     onSound.start();
+    public void switchOn(View view) {
+        on = (ImageButton) findViewById(R.id.light_switch_off);
+        on.setVisibility(View.INVISIBLE);
+        onSound = MediaPlayer.create(this, R.raw.open_light_switch_sound);
+        onSound.start();
     }
 
-    public void switchOff(View view)
-    {
+    public void switchOff(View view) {
         on.setVisibility(View.VISIBLE);
-        offSound = MediaPlayer.create(this, R.raw.close_light_switch_sound);
+        offSound = MediaPlayer.create(this, R.raw.open_light_switch_sound);
         offSound.start();
     }
 }
